3934d38e40ff928a24c98ec5d1132bbc3de820dd
Manoj Santra <santramanoj1997@gmail.com>
2023-09-09 20:46:16 +0530

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='mx-5'>
+    this is a protected routes
+    </div>
+  )
+}
